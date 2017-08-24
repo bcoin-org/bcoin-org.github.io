22ade603ca2aa361566e7393459c524055514d63
Bucko <bucko.perley@gmail.com>
2017-08-23 17:42:57 -0700

    npm script for running markdown converter

--- guide-generator.js
+++ guide-generator.js
@@ -4,26 +4,72 @@ const path = require('path');
 
 const renderer = new marked.Renderer();
 
-renderer.code = function(code, language) {
-  return `<pre class="line-numbers"><code class="${language}">${code}</code></pre>`;
+const guidesDir = path.resolve(__dirname, 'guides');
+const markdownDir = path.resolve(__dirname, 'guides/markdown/');
+
+const args = process.argv.slice(2);
+let author, file;
+
+args.forEach(arg => {
+  if (arg.indexOf('--author=') > -1) {
+    author = arg.slice(arg.indexOf('=') +1);
+  } else if (arg.indexOf('--file=') > -1) {
+    file = arg.slice(arg.indexOf('=') + 1);
+  }
+});
+
+if ( !author || !file ) {
+  console.log('Please pass an argument for author and file');
+  console.log('options:');
+  console.log('\'--author\'    e.g. `--author="JOHN SNOW"`');
+  console.log('\'--file\'      e.g. `--file="my-markdown.md"`');
+  console.log('Make sure the file is in the markdown directory in guides');
+  return;
+} else if (!fs.existsSync(path.resolve(markdownDir, file))) {
+    console.log('That markdown file does not exist!');
+    return;
+}
+
+const markdownFile = path.resolve(markdownDir, file);
+const htmlFile = path.resolve(guidesDir, file.replace(/\.[^/.]+$/, ".html"));
+
+/******
+Prepare the HTML FIlE
+******/
+renderer.heading = (text, level) => {
+  if (level == '2' || level == '1' ) {
+    return '<h2 class="post-title panel-title">'
+           + text + '</h2>'
+           + '<ul class="post-meta">' 
+           + '<li class="author">By ' + author + '</li>'
+           + '</ul>';
+  } else {
+    return `<h${level}>${text}</h${level}>`;
+  }
 }
 
 marked.setOptions({
   renderer,
   gfm: true,
-  highlight: function (code,lang) {
-    return require('highlight.js').highlightAuto(code).value;
-  } 
+  highlight: function (code, lang) {
+      return require('node-prismjs').highlight(code, Prism.languages[lang]);
+    }
 });
 
 
-const markdownString = fs.readFileSync('./guides/markdown/test-guide.md', 'utf8');
+const markdownString = fs.readFileSync(markdownFile, 'utf8');
 
-const blogText = marked(markdownString);
+// Assemble guide text container
+let blogText = '<div class="post-content panel-heading" style="color:#000">'; 
+blogText += marked(markdownString);
+blogText += '</div>'
 
-const template = fs.readFileSync('./guides/markdown/guides-template.txt').toString().split('\n');
-const startText = 'START OF GUIDE';
+// Get the guide html template and find start of guide section
+const template = fs.readFileSync(path.resolve(markdownDir, 'guides-template.txt'))
+                    .toString().split('\n');
+const startText = 'START OF GUIDE'; // NOTE: Make sure to change this if the comment text changes
 let startLine = 0;
+
 for (let i=0; i <= template.length; i++) {
   if (template[i].indexOf(startText) > -1) {
     startLine = i + 1;
@@ -33,7 +79,7 @@ for (let i=0; i <= template.length; i++) {
 
 template.splice(startLine, 0, blogText);
 
-fs.writeFile('./guides/test.html', template.join('\n'), err => {
+fs.writeFile(htmlFile, template.join('\n'), err => {
   if (err) throw err;
   console.log('File done!');
 });
--- guides/markdown/test-guide.md
+++ guides/markdown/test-guide.md
@@ -4,5 +4,24 @@ I'm just testing some markdown right now
 
 ```javascript
 console.log('hello world!');
+const bcoin = require('bcoin');
+// do cool stuff with bcoin!
+// https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses
+
+secp256k1 = require("bcoin/lib/crypto/secp256k1")
+crypto = require("bcoin/lib/crypto")
+base58 = require("bcoin/lib/utils/base58")
+
+// 0 - Having a private ECDSA key
+privkey = buffer.Buffer.from("18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725", "hex")
+
+// 1 - Take the corresponding public key generated with it (65 bytes, 1 byte 0x04, 32 bytes corresponding to X coordinate, 32 bytes corresponding to Y coordinate)
+pubkey = secp256k1.publicKeyCreate(privkey, false)
+
+// 2 - Perform SHA-256 hashing on the public key
+step2 = crypto.sha256(pubkey)
+...
 ```
-#### Cool headers too
+### Embeded gist?
+<script src="https://gist.github.com/tuxcanfly/23f77cfcc9fe6e61b67d38c8d737e4ad.js"></script>
+#### This is an example of a fourth level header 
