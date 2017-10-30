deaa838db91c6f7a6793a3dd9ecc047590fc2276
Node <nodar.chkuaselidze@gmail.com>
2017-10-30 23:02:41 +0400

    Fix build on osx

--- build-api.sh
+++ build-api.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd ./api-docs-slate
+
 gem install bundler
 bundle install
 bundle exec middleman build --clean # build api docs
-cp -RT ./build/ ../api-docs && rm -r ./build
+
+rm -r ../api-docs 2> /dev/null
+cp -R ./build/ ../api-docs && rm -r ./build
